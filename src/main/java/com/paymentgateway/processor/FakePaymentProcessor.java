e7db1037fc740fd3dcda2bc5db5e6c6e521c9acc
Gaurav Jain <gauravjain200024@gmail.com>
2026-09-22 15:20:41 +0530

    removed unnecesary code

--- src/main/java/com/paymentgateway/processor/FakePaymentProcessor.java
+++ src/main/java/com/paymentgateway/processor/FakePaymentProcessor.java
@@ -57,20 +57,16 @@ public class FakePaymentProcessor implements PaymentProcessor {
                 throw new PaymentDeclinedException();
 
             case "tok_timeout":
-                try {
-                    ProcessorPayment processorPayment = new ProcessorPayment();
 
-                    processorPayment.setStatus(generateProcessorOutcome());
-                    processorPayment.setMerchantReference(merchantReference);
-                    processorPayment.setProcessorTransactionId("TXN-" + UUID.randomUUID());
-                    // processorPayment.setOrderId(orderid);
+                ProcessorPayment processorPayment = new ProcessorPayment();
 
-                    processorPaymentRepository.save(processorPayment);
+                processorPayment.setStatus(generateProcessorOutcome());
+                processorPayment.setMerchantReference(merchantReference);
+                processorPayment.setProcessorTransactionId("TXN-" + UUID.randomUUID());
+                // processorPayment.setOrderId(orderid);
 
-                    Thread.sleep(3000); // simulate timeout scenario
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+                processorPaymentRepository.save(processorPayment);
+
                 throw new PaymentTimeoutException(null);
 
             default:
